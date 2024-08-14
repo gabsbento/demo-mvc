8c5fa9921b3be7715a91da0fe61d6c8bf4277661
Gabriel Bento <gab.silva.bento@gmail.com>
2024-08-14 16:24:14 -0300

    criada rota de edicao de departamentos

--- src/main/java/com/gabriel/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/gabriel/curso/boot/web/controller/DepartamentoController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gabriel.curso.boot.domain.Departamento;
 import com.gabriel.curso.boot.service.DepartamentoService;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/departamentos")
@@ -29,6 +32,16 @@ public class DepartamentoController {
 	@PostMapping("/salvar")
 	public String salvar(Departamento departamento) {
 		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("departamento", service.buscarPorId(id));
+		return "/departamento/cadastro";
+	}
+	@PostMapping("/editar")
+	public String editar(Departamento departamento) {
+		service.editar(departamento);
 		return "redirect:/departamentos/cadastrar";
 	}
 }
